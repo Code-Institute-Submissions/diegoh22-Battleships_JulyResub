875f2fb054d2027ffebb91d3d032412882ee0316
diegoh22 <diegohofficial@gmail.com>
2021-09-30 09:10:52 +0000

    first commit

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+GAME_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
