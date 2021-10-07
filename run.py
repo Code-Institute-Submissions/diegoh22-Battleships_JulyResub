21f4d6cf6710b0cb91b91b72ea2bf9211e6bd456
diegoh22 <diegohofficial@gmail.com>
2021-10-07 19:12:27 +0000

    add game logic

--- run.py
+++ run.py
@@ -8,3 +8,25 @@ curses.noecho()
 curses.curs_set(0)
 win.border(0)
 win.nodelay(1)
+
+# snake and food
+snake = [(4, 10), (4, 9), (4, 8)]
+food = (10, 20)
+
+win.addch(food[0], food[1], '#')
+
+# Game Logic
+score = 0
+
+ESC = 27
+key = curses.KEY_RIGHT
+
+
+while key != ESC:
+    win.addstr(0, 2, 'Score' + str(score) + '')
+    win.timeout(160 - (len(snake)) // 5 + len(snake)//10 % 120)
+    event = win.getch()
+
+
+curses.endwin()
+print(f"Final Score = {score}")
