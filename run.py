ada8cfe3a3eae74ce85bc4f88d33b302d4d2cc96
diegoh22 <diegohofficial@gmail.com>
2022-07-24 10:03:05 +0000

    first commint new code

--- run.py
+++ run.py
@@ -1,77 +1,50 @@
 from random import randint
 
-scores = {"computer": 0, "player": 0}
+class board:
 
-
-class Board:
-
-    def __init__(self, size, num_ships, name, type):
+   def __init__(self, size, num_ships, name, player=False):
         self.size = size
-        self.board = [["." for x in range(size)] for y in range(size)]
         self.num_ships = num_ships
-        self.type = type
-        self.guesses = []
+        self.name = name
+        self.player = player
         self.ships = []
+        self.guesses = []
+        self.populate()
 
-    def print(self):
+
+    def view_board(self):
+        
+        view_board(f"{self.name}'s Board:")
         for row in self.board:
-            print(" ".join(row))
+            print(" ".join(row))   
+
+    def add_ship(self):
+        """
+        Populates the board with ships
+        """
+        board = [["." for x in range(self.size)] for y in range(self.size)]
+        self.board = board
+        for _ in range(self.num_ships):
+            x, y = random_coordinate(self.size)
+            while (x, y) in self.ships:
+                x, y = random_coordinate(self.size)
+            self.ships.append((x, y))
+            if self.player:
+                self.board[x][y] = "@" 
 
     def guess(self, x, y):
+       
         self.guesses.append((x, y))
         self.board[x][y] = "X"
 
         if (x, y) in self.ships:
             self.board[x][y] = "*"
-            return "Hit"
+            return True
         else:
-            return "Miss"   
-
-    def add_ship(self, x, y, type="computer"):
-        if len(self.ships) >= self.num_ships:
-            print("Error: you cannot add any more ships!")
-        else:
-            self.ships.append((x, y))
-            if self.type == "player":
-                self.board[x][y] = "@"  
-
-def random_point(size):
-
-    return randint(0, size - 1)             
-
-def valid_coordinates(x, y, board):
-    pass
-
-def populate_board(board):
-    pass
-
-def make_guess(board):
-    pass
-
-def play_game(computer_board, player_board):
-    pass
-
-def new_game():
-    size = 7
-    num_ships = 5
-    scores["computer"] = 0
-    scores["palyer"] = 0
-    print("_" * 40)
-    print("WELCOME TO BATTLESHIPS")
-    print(f" Board size: {size}. Number of ships: {num_ships}")
-    print(" Top Left Corner is Row 0, col: 0")
-    print("_" * 40)
-    player_name = input("Enter your name: ")
-    print("-" * 40)
-
-    computer_board = Board(size, num_ships, "Computer", type="computer")
-    player_board = Board(size, num_ships, player_name, type="player")
-
-    for _ in range(num_ships):
-        populate_board(player_board)
-        populate_board(computer_board)
-
-    play_game(computer_board, player_board)    
-
-
-new_game()
+            return False  
+    
+    def already_guessed(self, x, y):
+    
+        if (x, y) in self.guesses:
+            return True
+        return False
