1ca6b01c7a36f707e109e71c5030a07bc76087a4
diegoh22 <diegohofficial@gmail.com>
2022-07-24 13:30:53 +0000

    add 2 clases and the def fot the coordinates

---
+++ run.py
@@ -0,0 +1,28 @@
+from random import randint
+
+
+def rand_coordinate(board_size):
+
+    x = randint(0, board_size - 1)
+    y = randint(0, board_size - 1)
+    return(x, y)
+
+def valid_coordinates(x, y, board_size)
+    pass:
+
+
+class Board:
+
+    pass
+
+
+
+
+class Game.play_game:
+    pass
+
+
+
+while True:
+
+    pass
